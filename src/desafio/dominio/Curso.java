7231939845d735ef02871c94790b423688897e25
LucasBS10 <lucasbernardo2604@gmail.com>
2023-04-06 22:24:18 -0300

    Add Mentoria e Curso

---
+++ src/desafio/dominio/Curso.java
@@ -0,0 +1,42 @@
+package desafio.dominio;
+
+public class Curso {
+    private String titulo,descricao;
+    private int cargoraria;
+
+    public Curso() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCargoraria() {
+        return cargoraria;
+    }
+
+    public void setCargoraria(int cargoraria) {
+        this.cargoraria = cargoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", cargoraria=" + cargoraria +
+                '}';
+    }
+}
