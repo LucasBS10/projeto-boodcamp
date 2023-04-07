df98ac5ecd49a1f6663490e757e712c8835cbdc0
LucasBS10 <lucasbernardo2604@gmail.com>
2023-04-06 23:26:27 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
-import desafio.dominio.Curso;
-import desafio.dominio.Mentoria;
+import desafio.dominio.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,22 +9,56 @@ public class Main {
         Curso curso1 = new Curso();
         curso1.setTitulo("curso java");
         curso1.setDescricao("descrição curso java");
-        curso1.setCargoraria(8);
+        curso1.setCargaHoraria(8);
 
         Curso curso2 = new Curso();
         curso2.setTitulo("curso js");
         curso2.setDescricao("descrição curso js");
-        curso2.setCargoraria(4);
-
+        curso2.setCargaHoraria(4);
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("mentoria de java");
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Boodcamp boodcamp = new Boodcamp();
+        boodcamp.setNome("Boodcamp Java Developer");
+        boodcamp.setDescricao("Descrição Java Developer");
+        boodcamp.getConteudos().add(curso1);
+        boodcamp.getConteudos().add(curso2);
+        boodcamp.getConteudos().add(mentoria);
+
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverBoodcamp(boodcamp);
+        System.out.println("Conteudos Inscritos Camila" + devCamila.getConteudosInscritos());
+        devCamila.progredir();
+        devCamila.progredir();
+        devCamila.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos Camila" + devCamila.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos Camila" + devCamila.getConteudosConcluidos());
+        System.out.println("XP: " + devCamila.calcularTotalXp());
+
+        System.out.println( "-------------" );
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("Joao");
+        devJoao.inscreverBoodcamp(boodcamp);
+        System.out.println("Conteudos Inscritos Joao" + devJoao.getConteudosInscritos());
+        devJoao.progredir();
+        devJoao.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos Joao" + devJoao.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos Joao" + devJoao.getConteudosConcluidos());
+        System.out.println("XP: " + devCamila.calcularTotalXp());
+
+
+
 
     }
 }
--- src/desafio/dominio/Curso.java
+++ src/desafio/dominio/Curso.java
@@ -1,42 +1,30 @@
 package desafio.dominio;
 
-public class Curso {
-    private String titulo,descricao;
-    private int cargoraria;
+public class Curso extends Conteudo{
+    private int cargaHoraria;
 
     public Curso() {
     }
 
-    public String getTitulo() {
-        return titulo;
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
     }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
+    public int getCargaHoraria() {
+        return cargaHoraria;
     }
 
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public int getCargoraria() {
-        return cargoraria;
-    }
-
-    public void setCargoraria(int cargoraria) {
-        this.cargoraria = cargoraria;
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
     }
 
     @Override
     public String toString() {
         return "Curso{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
-                ", cargoraria=" + cargoraria +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", cargoraria=" + cargaHoraria +
                 '}';
     }
 }
